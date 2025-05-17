776ceaada81501a37989a4bef3e57cb421683d97
BitDevOne <pracadanielmatysik@gmail.com>
2025-05-17 02:05:02 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,4 @@
+Start-stop 60 -seconds
 $config = Get-Content .\config.json | ConvertFrom-Json
 foreach ($app in $config.apps) {
     winget install --id $app -e --accept-source-agreements --accept-package-agreements
